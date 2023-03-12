d76e6815ab05f6f630899f91703c1609eea058a4
Zaki Orya <zakioria@gmail.com>
2023-03-12 15:13:40 +0300

    deploy

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -33,28 +33,7 @@ const Computers = React.memo(() => {
 });
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted [0.5]
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
+ 
 
   return (
     <section>
