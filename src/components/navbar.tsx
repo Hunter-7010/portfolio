fc791d3a2369acd7d334f4ea42cb2254f52c5218
Zaki Orya <zakioria@gmail.com>
2023-03-13 05:47:06 +0300

    added skills to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -60,12 +60,21 @@ const Navbar = () => {
                 About
               </a>
             </li>
+
             <li>
               <a
                 href="#experience"
                 className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white"
               >
                 Journey
+              </a>
+            </li>
+            <li>
+              <a
+                href="#skills"
+                className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white"
+              >
+                Skills
               </a>
             </li>
             <li>
