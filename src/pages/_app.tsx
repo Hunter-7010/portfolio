f126ca0d12f0094c9de838dfb4e852d1c43ed04d
Zaki Orya <zakioria@gmail.com>
2023-04-12 06:21:08 +0300

    adding umami script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,20 @@ import { type AppType } from "next/app";
 
 import { api } from "../utils/api";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <Script
+        async
+        defer
+        data-website-id="c439c72c-6bde-4c7b-a890-edb71d30a81b"
+        src="https://umami-production-f85b.up.railway.app/umami.js"
+      />
+
       <Component {...pageProps} />
       <Analytics />
     </>
