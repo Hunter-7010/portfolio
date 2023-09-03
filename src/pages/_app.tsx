d15a874ac60120cd036e2c49193d024662f4610f
Zaki Orya <zakioria@gmail.com>
2023-09-03 21:15:21 +0300

    added smooth scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,22 @@ import { type AppType } from "next/app";
 import { api } from "../utils/api";
 import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
-
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  //smooth scroll
+  useEffect(() => {
+    const lenis = new Lenis({lerp: 0.05,touchMultiplier:1 });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <Script
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { type NextPage } from "next";
 import { NextSeo } from "next-seo";
+import { motion, useMotionValue, useSpring } from "framer-motion";
+import { MouseEvent } from "react";
 import Head from "next/head";
 import Navbar from "../components/navbar";
 import Hero from "../components/hero";
@@ -12,48 +14,74 @@ import TawkTo from "../components/Tawk";
 import Skills from "../components/skills";
 import Footer from "../components/footer";
 const Home: NextPage = () => {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const physics = { damping: 120, stiffness: 700 };
+  const mouseX = useMotionValue(0);
+  const mouseXSpring = useSpring(mouseX);
+  const mouseY = useMotionValue(0);
+  const mouseYSpring = useSpring(mouseY);
+  const circleRadius = useMotionValue(64);
+  const circleRadiusSpring = useSpring(circleRadius);
 
+  function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+    const { left, top } = currentTarget.getBoundingClientRect();
+    mouseX.set(clientX - left - circleRadius.get() / 2);
+    mouseY.set(clientY - top - circleRadius.get() / 2);
+  }
   return (
     <>
-    <Head>
+      <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NextSeo
-      title="Zaki Orya"
-      description="This Portofolio is created using nextjs, react three fiber, three.js, framer motion and tailwind."
-      canonical="https://zakiorya.vercel.app/"
-      openGraph={{
-        url: 'https://zakiorya.vercel.app/',
-        title: 'Zaki Orya Portofolio',
-        description: 'I am an experienced software developer proficient in JavaScript, TypeScript, I have worked with frameworks like React, Next.js, Express',
-        images: [
-          {
-            url: 'https://res.cloudinary.com/dcajz16bn/image/upload/v1688406602/Screen_Shot_2023-07-03_at_8.49.11_PM_s0ccts.png',
-            width: 800,
-            height: 600,
-            alt: 'Site Preview',
-            type: 'image/png',
-          },
-        ],
-        siteName: 'Zaki Orya Portofolio',
-      }}
-      twitter={{
-        handle: '@ZakiOrya',
-        cardType: 'https://res.cloudinary.com/dcajz16bn/image/upload/v1688406602/Screen_Shot_2023-07-03_at_8.49.11_PM_s0ccts.png',
-      }}
-      robotsProps={{
-        nosnippet: true,
-        notranslate: true,
-        noimageindex: true,
-        noarchive: true,
-        maxSnippet: -1,
-        maxImagePreview: 'none',
-        maxVideoPreview: -1,
-      }}
-      
-    />
-      <section className="dark relative overflow-hidden scroll-smooth bg-gradient-to-t from-[#2A1445] to-[#512785] text-white">
+        title="Zaki Orya"
+        description="This Portofolio is created using nextjs, react three fiber, three.js, framer motion and tailwind."
+        canonical="https://zakiorya.vercel.app/"
+        openGraph={{
+          url: "https://zakiorya.vercel.app/",
+          title: "Zaki Orya Portofolio",
+          description:
+            "I am an experienced software developer proficient in JavaScript, TypeScript, I have worked with frameworks like React, Next.js, Express",
+          images: [
+            {
+              url: "https://res.cloudinary.com/dcajz16bn/image/upload/v1688406602/Screen_Shot_2023-07-03_at_8.49.11_PM_s0ccts.png",
+              width: 800,
+              height: 600,
+              alt: "Site Preview",
+              type: "image/png",
+            },
+          ],
+          siteName: "Zaki Orya Portofolio",
+        }}
+        twitter={{
+          handle: "@ZakiOrya",
+          cardType:
+            "https://res.cloudinary.com/dcajz16bn/image/upload/v1688406602/Screen_Shot_2023-07-03_at_8.49.11_PM_s0ccts.png",
+        }}
+        robotsProps={{
+          nosnippet: true,
+          notranslate: true,
+          noimageindex: true,
+          noarchive: true,
+          maxSnippet: -1,
+          maxImagePreview: "none",
+          maxVideoPreview: -1,
+        }}
+      />
+      <section className="dark cursor-n relative overflow-hidden scroll-smooth bg-gradient-to-t from-[#2A1445] to-[#512785] text-white">
+        <motion.div
+          onMouseMove={handleMouseMove}
+          className="fixed inset-0 h-screen w-screen z-[100]"
+        >
+          <motion.div
+            style={{
+              x: mouseXSpring,
+              y: mouseYSpring,
+              width: circleRadiusSpring,
+              height: circleRadiusSpring,
+            }}
+            className="fixed top-0 left-0 flex items-center justify-center rounded-full border-2 border-dashed border-purple-500 backdrop-saturate-200"
+          />
+        </motion.div>
         <Navbar />
         <Hero />
         <About />
@@ -61,7 +89,7 @@ const Home: NextPage = () => {
         <Skills />
         <Projects />
         <Contact />
-        <Footer/>
+        <Footer />
         <TawkTo />
       </section>
     </>
